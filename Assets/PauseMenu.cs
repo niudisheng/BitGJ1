b9c13ef0551163fd609ef82b6d9a026b7fe75c0c
nokanowayi <962245699@qq.com>
2024-11-24 21:28:53 +0800

    ui

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+    }
+    public void BackToGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
