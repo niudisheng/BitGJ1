7be26887824fb73214f770b9a786d5af59dd0884
niudisheng <106807597+niudisheng@users.noreply.github.com>
2024-11-27 02:06:01 +0800

    UI管理与事件设定

---
+++ Assets/Script/Manager/UIManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject canvas;
+    public List<GameObject> uiList;
+    Dictionary<UIType,GameObject> uiDictionary;
+    private void Awake()
+    {
+        for (int i = 0; i < uiList.Count; i++)
+        {
+            UIType uiType = (UIType)i;
+            uiDictionary.Add(uiType,uiList[i]);
+        }
+    }
+
+    public void ShowCanvas(bool show)
+    {
+        canvas.SetActive(show);
+    }
+
+    public void ShowAllUI()
+    {
+        setUIActive(UIType.All,true);
+    }
+
+    public void HideAllUI()
+    {
+        setUIActive(UIType.All,false);
+        
+    }
+
+    public void setUIActive(UIType uiType,bool active)
+    {
+        if (uiType == UIType.All)
+        {
+            foreach (var UI in uiList)
+            {
+                UI.SetActive(active);
+            }
+        }
+        GameObject ui = uiDictionary[uiType];
+        ui.SetActive(active);
+    }
+}
