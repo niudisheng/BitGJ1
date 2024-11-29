8925a521c1d3f8b977515e696d2b53653e969019
nokanowayi <962245699@qq.com>
2024-11-30 00:14:30 +0800

    电子锁2

---
+++ Assets/Script/Items/CodeBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeBox : MonoBehaviour
+{
+    public List<GameObject> gameObjects = new List<GameObject>();
+    public List<Sprite> nowSprite = new List<Sprite>();
+    public List<Sprite> answer = new List<Sprite>();
+    private void Update()
+    {
+        nowSprite[0] = gameObjects[0].GetComponent<SpriteRenderer>().sprite;
+        nowSprite[1] = gameObjects[1].GetComponent<SpriteRenderer>().sprite;
+        nowSprite[2] = gameObjects[2].GetComponent<SpriteRenderer>().sprite;
+        nowSprite[3] = gameObjects[3].GetComponent<SpriteRenderer>().sprite;
+        if (nowSprite[0] == answer[0] & nowSprite[1] == answer[1] & nowSprite[2] == answer[2] & nowSprite[3] == answer[3])
+        {
+            SceneLoadManager.LoadScene("CodeBox2");
+        }
+    }
+}
